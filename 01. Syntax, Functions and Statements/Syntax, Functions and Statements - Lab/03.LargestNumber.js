703e2c01e265aa818044d0fbecf1805ea8234233
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2022-09-14 19:58:53 +0300

    Add a solution

---
+++ 01. Syntax, Functions and Statements/Syntax, Functions and Statements - Lab/03.LargestNumber.js
@@ -0,0 +1,17 @@
+function largestNumber(num1, num2, num3){
+
+    let largest;
+
+    if(num1 > num2 && num1 > num3){
+        largest = num1;
+    } else if(num2 > num1 && num2 > num3){
+        largest = num2;
+    } else{
+        largest = num3;
+    }
+
+    console.log(`The largest number is ${largest}.`)
+}
+
+largestNumber(5, -3, 16);
+largestNumber(-3, -5, -22.5);
