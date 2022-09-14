efa30753f12bb22aba0013031cbaac247bc8be74
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2022-09-14 20:20:38 +0300

    Add a solution

---
+++ 01. Syntax, Functions and Statements/Syntax, Functions and Statements - Lab/05.MathOperations.js
@@ -0,0 +1,18 @@
+function mathOperator(num1, num2, operator){
+
+    let result;
+
+    switch(operator){
+        case '+': result = num1 + num2; break;
+        case '-': result = num1 - num2; break;
+        case '*': result = num1 * num2; break;
+        case '/': result = num1 / num2; break;
+        case '%': result = num1 % num2; break;
+        case '**': result = num1 ** num2; break;
+    }
+
+    console.log(result);
+}
+
+mathOperator(5, 6, '+');
+mathOperator(3, 5.5, '*');
