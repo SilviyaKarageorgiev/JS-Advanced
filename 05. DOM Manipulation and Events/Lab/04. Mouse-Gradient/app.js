9110de905086e73e8313996e0cf111658da09733
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2022-10-08 13:34:53 +0300

    Add

---
+++ 05. DOM Manipulation and Events/Lab/04. Mouse-Gradient/app.js
@@ -0,0 +1,10 @@
+function attachGradientEvents() {
+
+    let gradient = document.getElementById('gradient');
+    gradient.addEventListener('mousemove', onMouseMove);
+    let result = document.getElementById('result');
+
+    function onMouseMove(ev) {
+        result.textContent = Math.floor(ev.offsetX / gradient.clientWidth * 100) + '%';
+    }
+}
