43a082ff4a278f4b6f2abae52b8699a49be7f09f
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2022-10-12 09:56:55 +0300

    Add

--- 05. DOM Manipulation and Events/Exercise/04. Fill-Dropdown/dropdown.js
+++ 05. DOM Manipulation and Events/Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,13 +1,15 @@
 function addItem() {
-    
+
     let newItemText = document.getElementById('newItemText');
     let newItemValue = document.getElementById('newItemValue');
 
     let menu = document.getElementById('menu');
     let optionItem = document.createElement('option');
-    
+
+    optionItem.textContent = newItemText.value;
+    optionItem.value = newItemValue.value;
     menu.appendChild(optionItem);
-    optionItem.textContent = `${newItemText.value} ${newItemValue.value}`;
+
     newItemText.value = '';
     newItemValue.value = '';
 }
