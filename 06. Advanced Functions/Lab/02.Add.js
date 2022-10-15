8a3399b7abb857170347751d8da6b93fdd7391a9
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2022-10-15 23:27:58 +0300

    Add

--- 06. Advanced Functions/Lab/02.Add.js
+++ 06. Advanced Functions/Lab/02.Add.js
@@ -4,8 +4,14 @@ function solution(number) {
         return a + b;
     }
     return add.bind(this, number);
+
+    //return (a) => a + number;
 }
 
 let add5 = solution(5);
 console.log(add5(2));
 console.log(add5(3));
+
+let add7 = solution(7);
+console.log(add7(2));
+console.log(add7(3));
