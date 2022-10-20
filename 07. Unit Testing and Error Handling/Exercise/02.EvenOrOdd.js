4eca8c772ea560f8cea1897cea2775f2325db22d
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2022-10-20 09:54:02 +0300

    Add

---
+++ 07. Unit Testing and Error Handling/Exercise/02.EvenOrOdd.js
@@ -0,0 +1,12 @@
+module.exports = { isOddOrEven };
+
+function isOddOrEven(string) {
+    if (typeof (string) !== 'string') {
+        return undefined;
+    }
+    if (string.length % 2 === 0) {
+        return "even";
+    }
+
+    return "odd";
+}
